e561ccef295f9a84ed20149ed2a92df4121dee14
driver166 <blibilihlbdx@outlook.com>
2026-02-15 18:24:32 +0800

    Update 1.16.x

--- src/main/java/com/mod/driver166/BetterCrashReports.java
+++ src/main/java/com/mod/driver166/BetterCrashReports.java
@@ -2,10 +2,8 @@ package com.mod.driver166;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
-import net.minecraft.server.network.ServerPlayerEntity;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class BetterCrashReports implements ModInitializer {
 	public static final String MOD_ID = "better-crash-reports";
@@ -13,7 +11,7 @@ public class BetterCrashReports implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
