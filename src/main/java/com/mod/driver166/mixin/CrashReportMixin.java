a177da7d00b831228ace28d0f6c60332507f813d
driver166 <blibilihlbdx@outlook.com>
2026-02-21 16:44:42 +0800

    Update v1.1.0

--- src/main/java/com/mod/driver166/mixin/CrashReportMixin.java
+++ src/main/java/com/mod/driver166/mixin/CrashReportMixin.java
@@ -3,8 +3,12 @@ package com.mod.driver166.mixin;
 import com.google.common.collect.Lists;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
+<<<<<<< Updated upstream
 import net.fabricmc.loader.api.metadata.ModMetadata;
 import net.fabricmc.loader.api.metadata.Person;
+=======
+import net.fabricmc.loader.api.metadata.*;
+>>>>>>> Stashed changes
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.SystemDetails;
 import net.minecraft.util.Util;
@@ -21,21 +25,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Optional;
+import java.util.*;
 
 @Mixin(CrashReport.class)
 public abstract class CrashReportMixin {
     @Shadow
     public abstract String getMessage();
     @Shadow
-    public abstract String getCauseAsString();
-    @Shadow
     private final List<CrashReportSection> otherSections = Lists.newArrayList();
     @Shadow
     private StackTraceElement[] stackTrace = new StackTraceElement[0];
+    @Shadow
+    public abstract String getCauseAsString();
     @Shadow
     private final SystemDetails systemDetailsSection = new SystemDetails();
 
@@ -122,14 +123,26 @@ public abstract class CrashReportMixin {
         return sb.toString();
     }
     @Unique
+<<<<<<< Updated upstream
     private void generate_advertise(StringBuilder stringBuilder){
+=======
+    private void generate_advise(StringBuilder stringBuilder){
+>>>>>>> Stashed changes
         stringBuilder.append("\n").append(new TranslatableText("crash.better-crash-reports.adv").getString()).append("\n");
         String[] lines = getCauseAsString().split("\n");
+        String[] advises = new String[]{
+                "crash.better-crash-reports.adv_a.1",
+                "crash.better-crash-reports.adv_a.2",
+                "crash.better-crash-reports.adv_a.3",
+                "crash.better-crash-reports.adv_a.4",
+                "crash.better-crash-reports.adv_a.5"
+        };
         try {
             if (lines[0].contains("provided by")) {
                 if (mods_contains_modid(lines[0].split("provided by")[1].split(" at")[0].split("'")[1])) {
                     String mod_id = lines[0].split("provided by")[1].split(" at")[0].split("'")[1];
                     ModMetadata metadata = getModMetadata(mod_id);
+<<<<<<< Updated upstream
                     stringBuilder.append(new TranslatableText("crash.better-crash-reports.adv_a", getModName(lines[0].split("provided by")[1].split(" at")[0].split("'")[1])).getString());
                     stringBuilder.append("\n");
                     stringBuilder.append("--- ").append(new TranslatableText("crash.better-crash-reports.mod_info").getString()).append(" ---\n");
@@ -140,10 +153,25 @@ public abstract class CrashReportMixin {
                     stringBuilder.append(new TranslatableText("crash.better-crash-reports.mod_info.description").getString()).append(metadata.getDescription());
                 }
             } else if (lines[0].contains("Manually triggered debug crash") || lines[1].contains("pollDebugCrash")) {
+=======
+                    stringBuilder.append(new TranslatableText(advises[(int)(Util.getMeasuringTimeNano() % (long)advises.length)], getModName(lines[0].split("provided by")[1].split(" at")[0].split("'")[1])).getString());
+                    stringBuilder.append("\n");
+                    if (metadata != null) {
+                        stringBuilder.append("--- ").append(new TranslatableText("crash.better-crash-reports.mod_info").getString()).append(" ---\n");
+                        stringBuilder.append(new TranslatableText("crash.better-crash-reports.mod_info.name").getString()).append(getModName(mod_id)).append("\n");
+                        stringBuilder.append(new TranslatableText("crash.better-crash-reports.mod_info.mod_id").getString()).append(metadata.getId()).append("\n");
+                        stringBuilder.append(new TranslatableText("crash.better-crash-reports.mod_info.version").getString()).append(metadata.getVersion()).append("\n");
+                        stringBuilder.append(new TranslatableText("crash.better-crash-reports.mod_info.author").getString()).append(getAuthorInString(metadata)).append("\n");
+                        stringBuilder.append(new TranslatableText("crash.better-crash-reports.mod_info.description").getString()).append(metadata.getDescription());
+                    }
+                }
+            } else if (lines[0].contains("Manually triggered debug crash")) {
+>>>>>>> Stashed changes
                 stringBuilder.append(new TranslatableText("crash.better-crash-reports.adv_b").getString());
             } else if (mods_contains_modid(lines[1].split("\\$")[lines[1].split("\\$").length - 2])) {
                 String mod_id = lines[1].split("\\$")[lines[1].split("\\$").length - 2];
                 ModMetadata metadata = getModMetadata(mod_id);
+<<<<<<< Updated upstream
                 stringBuilder.append(new TranslatableText("crash.better-crash-reports.adv_a", getModName(lines[1].split("\\$")[lines[1].split("\\$").length - 2])).getString());
                 stringBuilder.append("\n");stringBuilder.append("--- ").append(new TranslatableText("crash.better-crash-reports.mod_info").getString()).append(" ---\n");
                 stringBuilder.append(new TranslatableText("crash.better-crash-reports.mod_info.name").getString()).append(getModName(mod_id)).append("\n");
@@ -151,6 +179,19 @@ public abstract class CrashReportMixin {
                 stringBuilder.append(new TranslatableText("crash.better-crash-reports.mod_info.version").getString()).append(metadata.getVersion()).append("\n");
                 stringBuilder.append(new TranslatableText("crash.better-crash-reports.mod_info.author").getString()).append(getAuthorInString(metadata)).append("\n");
                 stringBuilder.append(new TranslatableText("crash.better-crash-reports.mod_info.description").getString()).append(metadata.getDescription());
+=======
+                Random random = new Random();
+                stringBuilder.append(new TranslatableText(advises[random.nextInt(5)], getModName(mod_id)).getString());
+                if (metadata != null) {
+                    stringBuilder.append("\n");
+                    stringBuilder.append("--- ").append(new TranslatableText("crash.better-crash-reports.mod_info").getString()).append(" ---\n");
+                    stringBuilder.append(new TranslatableText("crash.better-crash-reports.mod_info.name").getString()).append(getModName(mod_id)).append("\n");
+                    stringBuilder.append(new TranslatableText("crash.better-crash-reports.mod_info.mod_id").getString()).append(metadata.getId()).append("\n");
+                    stringBuilder.append(new TranslatableText("crash.better-crash-reports.mod_info.version").getString()).append(metadata.getVersion()).append("\n");
+                    stringBuilder.append(new TranslatableText("crash.better-crash-reports.mod_info.author").getString()).append(getAuthorInString(metadata)).append("\n");
+                    stringBuilder.append(new TranslatableText("crash.better-crash-reports.mod_info.description").getString()).append(metadata.getDescription());
+                }
+>>>>>>> Stashed changes
             } else {
                 stringBuilder.append(new TranslatableText("crash.better-crash-reports.unknown").getString());
             }
@@ -174,12 +215,16 @@ public abstract class CrashReportMixin {
         sb.append(new TranslatableText("crash.better-crash-reports.thread").getString()).append(Thread.currentThread().getName()).append("\n");
         sb.append(new TranslatableText("crash.better-crash-reports.stacktrace").getString());
         sb.append("\n");
+<<<<<<< Updated upstream
         sb.append(getCauseAsString());
+=======
+        sb.append(getModifiedCauseAsString());
+>>>>>>> Stashed changes
         sb.append("\n\n").append(new TranslatableText("crash.better-crash-reports.mod_used").getString()).append("\n");
         for (ModContainer mod:FabricLoader.getInstance().getAllMods()){
             sb.append("\t- ").append(mod.toString()).append("\n");
         }
-        generate_advertise(sb);
+        generate_advise(sb);
         sb.append("\n\n");
         sb.append(new TranslatableText("crash.better-crash-reports.other_infos").getString());
         sb.append("\n");
@@ -198,6 +243,41 @@ public abstract class CrashReportMixin {
 
         cir.setReturnValue(sb.toString());
     }
+<<<<<<< Updated upstream
+=======
+    @Unique
+    private String joinListWithSeparator(List<String> list, @Nullable String separator){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String element : list){
+            stringBuilder.append(element);
+            if (!element.equals(list.get(list.size() - 1))){
+                stringBuilder.append(separator);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    @Unique
+    private String getModifiedCauseAsString() {
+        String cause = getCauseAsString();
+        try{
+            String[] splitCause = cause.split("\n");
+            String cause_string = splitCause[0].split(": ")[0];
+            String modified_cause_string = cause_string + (new TranslatableText(cause_string).getString().equals(cause_string) ? "" : "(" + new TranslatableText(cause_string).getString() + ")");
+            if (splitCause[0].split(": ").length != 1){
+                List<String> causeList = Arrays.asList(splitCause[0].split(": "));
+                causeList.set(0, modified_cause_string);
+                String causeFirstLine = joinListWithSeparator(causeList, ": ");
+                List<String> modifiedSplitCause = Arrays.asList(splitCause);
+                modifiedSplitCause.set(0, causeFirstLine);
+                cause = joinListWithSeparator(modifiedSplitCause, "\n");
+            }
+        } catch (IndexOutOfBoundsException ignored){
+        }
+        return cause;
+    }
+
+>>>>>>> Stashed changes
 
     static {
         DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.ROOT);
