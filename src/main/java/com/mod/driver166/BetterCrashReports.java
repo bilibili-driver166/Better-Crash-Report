237fbbedaa92c7aedbfcad5aee38a9f65a6fa53f
driver166 <blibilihlbdx@outlook.com>
2026-02-26 11:08:00 +0800

    Update v1.2.0

--- src/main/java/com/mod/driver166/BetterCrashReports.java
+++ src/main/java/com/mod/driver166/BetterCrashReports.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+
 public class BetterCrashReports implements ModInitializer {
 	public static final String MOD_ID = "better-crash-reports";
 
--- src/main/java/com/mod/driver166/mixin/CrashTest.java
+++ src/main/java/com/mod/driver166/mixin/CrashTest.java
@@ -1,15 +1,19 @@
 package com.mod.driver166.mixin;
 
+import net.minecraft.client.world.ClientChunkManager;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.chunk.ChunkStatus;
+import net.minecraft.world.chunk.WorldChunk;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(MinecraftServer.class)
+@Mixin(ClientChunkManager.class)
 public class CrashTest {
-    @Inject(at = @At(value = "HEAD"), method = "loadWorld")
-    public void loadWorld(CallbackInfo ci){
+    @Inject(at = @At(value = "HEAD"), method = "getChunk(IILnet/minecraft/world/chunk/ChunkStatus;Z)Lnet/minecraft/world/chunk/WorldChunk;", cancellable = true)
+    public void loadWorld(int i, int j, ChunkStatus chunkStatus, boolean bl, CallbackInfoReturnable<WorldChunk> cir){
         throw new NullPointerException("This is a test crash. If you see this in your normal play, please contact mod author.");
     }
 }
